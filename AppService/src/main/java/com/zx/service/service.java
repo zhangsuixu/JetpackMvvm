ea3f804908bfe3104bb4c1453cf0366308f53f8c
13922441990 <13922441990@163.com>
2021-09-05 10:18:49 +0800

    项目初创

---
+++ AppService/src/main/java/com/zx/service/service.java
@@ -0,0 +1,8 @@
+package com.zx.service;
+
+/**
+ * ZhangSuiXu
+ * 2021/9/5
+ */
+public class service {
+}
---
+++ AppUI/src/main/java/com/zx/ui/ui.java
@@ -0,0 +1,8 @@
+package com.zx.ui;
+
+/**
+ * ZhangSuiXu
+ * 2021/9/5
+ */
+public class ui {
+}
